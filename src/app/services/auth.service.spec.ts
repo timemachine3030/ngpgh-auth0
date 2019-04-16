1ebd01ba5582d11fbbf24d829b88b42333b2728b
Daniel Lopretto <dan@meterfeeder.com>
2019-04-16 15:45:45 -0400

    Get User profile from Auth0.

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-import {Auth0Callback, Auth0DecodedHash, Auth0ParseHashError, WebAuth} from 'auth0-js';
+import {Auth0Callback, Auth0DecodedHash, Auth0ParseHashError, Auth0UserProfile, WebAuth} from 'auth0-js';
+import {environment} from '../../environments/environment';
 
 import { AuthService } from './auth.service';
 
@@ -9,6 +10,8 @@ describe('AuthService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     window.localStorage.removeItem('auth_token');
+    window.localStorage.removeItem('expires_at');
+    window.localStorage.removeItem('profile');
     service = TestBed.get(AuthService);
   });
 
@@ -22,6 +25,8 @@ describe('AuthService', () => {
     });
 
     it(`must return true if auth token in the session`, () => {
+      const futureTime = Date.now() + 10000; // tn seconds in the future
+      window.localStorage.setItem('expires_at', JSON.stringify(futureTime));
       window.localStorage.setItem('auth_token', 'some token string');
       expect(service.isAuthenticated()).toBeTruthy();
     });
@@ -45,6 +50,62 @@ describe('AuthService', () => {
         await service.decodeAuthHash();
       } catch (msg) {
         expect(msg.error).toBe('problem');
+        return;
+      }
+      throw new Error('Promise should not be resolved');
+    });
+  });
+  describe('getUserProfile', () => {
+    it('saves the profile to the session', async () => {
+      const testProfile: Auth0UserProfile = {
+        created_at: '2018-04-04T17:39:47.337Z',
+        updated_at: '2019-04-10T20:12:17.650Z',
+
+        clientID: environment.auth.clientID,
+        identities: [{
+          user_id: '5ac50de3a33e56128a53aaa',
+          provider: 'auth0',
+          connection: 'Username-Password-Authentication',
+          isSocial: false,
+        }],
+
+        name: 'Sam Pull',
+        nickname: 'sammy',
+        picture: 'http://example.com/image.png',
+        sub: 'auth0|5ac50de3a33e56128a53aaa',
+        user_id: 'auth0|5ac50de3a33e56128a53aaa',
+
+      };
+      spyOn(Object.getPrototypeOf(service.auth0.client), 'userInfo')
+        .and.callFake((accessToken: string, cb: Auth0Callback<Auth0UserProfile>) => {
+        return cb(null, testProfile);
+      });
+      try {
+        const profile: Auth0UserProfile | null = await service.getUserProfile({
+          accessToken: 'the token string',
+          expiresIn: 15
+        });
+
+        expect(profile.name).toBe('Sam Pull');
+        const expectedExpire: Date = new Date();
+        expectedExpire.setTime(Date.now() + 15000);
+        expect(service.expiresAt).toEqual(expectedExpire, 'Token expires in 15 seconds');
+      } catch (err) {
+        expect(err).toBeFalsy();
+      }
+    });
+    it('rejects any errors', async () => {
+
+      spyOn(Object.getPrototypeOf(service.auth0.client), 'userInfo')
+        .and.callFake((accessToken: string, cb: Auth0Callback<Auth0UserProfile>) => {
+          return cb({error: 'invalid_token', errorDescription: 'a big problem'}, null);
+        });
+      try {
+        await service.getUserProfile({
+          accessToken: 'some token'
+        });
+      } catch (msg) {
+        expect(msg.error).toBe('invalid_token');
         return;
       }
       throw new Error('Promise should not be resolved');
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Auth0DecodedHash, Auth0ParseHashError, WebAuth} from 'auth0-js';
+import {Auth0DecodedHash, Auth0ParseHashError, Auth0UserProfile, WebAuth} from 'auth0-js';
 import {environment} from '../../environments/environment';
 
 @Injectable({
@@ -7,7 +7,11 @@ import {environment} from '../../environments/environment';
 })
 export class AuthService {
 
-  private auth0: WebAuth;
+  public auth0: WebAuth;
+  public expiresAt: Date;
+  public profile: Auth0UserProfile;
+
+  private authToken: string;
 
   constructor() {
     this.auth0 = new WebAuth({
@@ -21,7 +25,13 @@ export class AuthService {
   }
 
   public isAuthenticated(): boolean {
-    return !!window.localStorage.getItem('auth_token');
+    this.loadSession();
+    try {
+      const expired = Date.now() > this.expiresAt.getTime();
+      return this.authToken && !expired;
+    } catch (err) {
+      return false;
+    }
   }
 
   public decodeAuthHash(): Promise<Auth0DecodedHash | null> {
@@ -33,5 +43,49 @@ export class AuthService {
         return resolve(decoded);
       });
     }));
+  }
+
+  /**
+   * Use access token to retrieve user's profile from auth0 and set session
+   */
+  public getUserProfile(authResult: Auth0DecodedHash): Promise<Auth0UserProfile> {
+    return new Promise((resolve, reject) => {
+      this.auth0.client.userInfo(authResult.accessToken, (err: any, profile: Auth0UserProfile) => {
+        if (err) {
+          return reject(err);
+        }
+        this.setSession(authResult, profile);
+        return resolve(profile);
+      });
+    });
+  }
+
+  /**
+   * Populate instance properties from LocalStorage;
+   */
+  private loadSession() {
+    this.authToken = window.localStorage.getItem('auth_token');
+    this.profile = JSON.parse(window.localStorage.getItem('profile'));
+    const expireStr = window.localStorage.getItem('expires_at');
+    if (expireStr) {
+      const expireInt =  parseInt(expireStr, 10);
+      this.expiresAt = new Date(expireInt);
+    } else {
+      // in no item in storage, always expired
+      this.expiresAt = new Date(0);
+    }
+  }
+  /**
+   * Update the local storage information for the current login session.
+   */
+  private setSession(authResult: Auth0DecodedHash, profile: Auth0UserProfile): void {
+    // Set the time that the Access Token will expire at
+    const expiresInt = (authResult.expiresIn * 1000) + new Date().getTime();
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('expires_at', JSON.stringify(expiresInt));
+    localStorage.setItem('profile', JSON.stringify(profile));
+
+    // Load into the current instance
+    this.loadSession();
   }
 }
